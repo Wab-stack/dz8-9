ae831654a7532f03e34d905749a24d6279284e1d
Wab-stack <3216kirito1632@gmail.com>
2025-12-12 13:06:20 +0000

    nf

---
+++ app.py
@@ -0,0 +1,13 @@
+def Greet(name: str):
+    return f"hello, {name}!"
+
+def Sum(a: int, b: int):
+    return a+b
+
+def mul(a: int, b: int):
+    return a*b
+
+if __name__ == "__main__":
+    print(Greet("TIM"))
+    print(Sum(70,636))
+    print(mul(17,15))
